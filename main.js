b66deaa32959f0396c02c284fe13993d6abc588d
spdlqj7791@naver.com <spdlqj7791@naver.com>
2021-10-12 09:35:23 +0900

    router - split the homepage into files

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var express = require('express')
 var app = express()
 var port = 3000
 var fs = require('fs');
-var template = require('./lib/template.js');
 var compression = require('compression');
+var indexRouter = require('./routes/index');
 var topicRouter = require('./routes/topic');
 
 // serving static files (image in 'unsplash')
@@ -20,22 +20,8 @@ app.get('*', (request, response, next) => {
   });
 })
 
+app.use('/', indexRouter);
 app.use('/topic', topicRouter);
-
-app.get('/', (request, response) => {
-  var title = 'Welcome';
-  var description = 'Hello, Node.js';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list,
-    `<h2>${title}</h2>${description}
-    <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
-    `,
-    `<a href="/topic/create">create</a>`
-  );
-  response.send(html);
-})
-
-
 
 // error handling middleware
 app.use((request, response, next) => {
